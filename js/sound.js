cfd6ad28e88eea8fb32b8995a0a3651971598ebf
mujebpc-hub <mujebpc@gmail.com>
2026-06-17 19:21:51 +0530

    Update sound.js

--- js/sound.js
+++ js/sound.js
@@ -1,1 +1,26 @@
+const rollSound = new Audio("assets/sounds/roll.mp3");
+const moveSound = new Audio("assets/sounds/move.mp3");
+const killSound = new Audio("assets/sounds/kill.mp3");
+const winSound = new Audio("assets/sounds/win.mp3");
+const clickSound = new Audio("assets/sounds/click.mp3");
+
+function playRollSound(){
+rollSound.play();
+}
+
+function playMoveSound(){
+moveSound.play();
+}
+
+function playKillSound(){
+killSound.play();
+}
+
+function playWinSound(){
+winSound.play();
+}
+
+function playClickSound(){
+clickSound.play();
+}
 
