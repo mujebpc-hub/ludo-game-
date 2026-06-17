36b5349385a9e9747663899d135b81966f61468c
mujebpc-hub <mujebpc@gmail.com>
2026-06-17 19:08:20 +0530

    Update dice.js

--- js/dice.js
+++ js/dice.js
@@ -1,1 +1,19 @@
+const dice = document.getElementById("dice");
+
+let diceValue = 1;
+
+function rollDice(){
+diceValue = Math.floor(Math.random() * 6) + 1;
+
+```
+dice.src = `assets/images/dice/dice-${diceValue}.png`;
+
+return diceValue;
+```
+
+}
+
+dice.onclick = () => {
+rollDice();
+};
 
