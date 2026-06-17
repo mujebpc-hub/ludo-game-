fed700d622a1ba7548ae959fe5fdb969ef857da9
mujebpc-hub <mujebpc@gmail.com>
2026-06-17 19:14:34 +0530

    Update board.js

--- js/board.js
+++ js/board.js
@@ -1,1 +1,36 @@
+const tokensLayer = document.getElementById("tokens-layer");
+
+function createTokens(){
+players.forEach(player => {
+player.tokens.forEach(token => {
+const tokenElement = document.createElement("img");
+
+```
+        tokenElement.src = `assets/images/tokens/${player.color}.png`;
+        tokenElement.classList.add("token");
+
+        tokenElement.dataset.player = player.id;
+        tokenElement.dataset.token = token.id;
+
+        tokenElement.style.position = "absolute";
+
+        // Temporary starting positions
+        if(player.color === "red"){
+            tokenElement.style.left = "80px";
+            tokenElement.style.top = `${80 + (token.id * 40)}px`;
+        }
+
+        if(player.color === "blue"){
+            tokenElement.style.right = "80px";
+            tokenElement.style.top = `${80 + (token.id * 40)}px`;
+        }
+
+        tokensLayer.appendChild(tokenElement);
+    });
+});
+```
+
+}
+
+createTokens();
 
