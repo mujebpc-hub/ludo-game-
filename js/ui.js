caf07417557e84a24e21d92e4d6b152344c01fd4
mujebpc-hub <mujebpc@gmail.com>
2026-06-17 19:17:08 +0530

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,22 @@
+const topPlayer = document.querySelector(".top-player");
+const bottomPlayer = document.querySelector(".bottom-player");
+
+function updateTurnUI(){
+// Reset highlight
+topPlayer.style.border = "none";
+bottomPlayer.style.border = "none";
+
+```
+// Highlight current player
+if(currentPlayer === 0){
+    bottomPlayer.style.border = "3px solid yellow";
+}else{
+    topPlayer.style.border = "3px solid yellow";
+}
+```
+
+}
+
+// Run at game start
+updateTurnUI();
 
