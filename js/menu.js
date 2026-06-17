ab65b5de7c43a3467d0c7e8df30935069c4c3890
mujebpc-hub <mujebpc@gmail.com>
2026-06-17 19:07:04 +0530

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,27 @@
+const menuScreen = document.getElementById("menu-screen");
+const gameScreen = document.getElementById("game-screen");
+
+const playBtn = document.getElementById("play-btn");
+const botBtn = document.getElementById("bot-btn");
+const onlineBtn = document.getElementById("online-btn");
+const backBtn = document.getElementById("back-btn");
+
+playBtn.onclick = () => {
+menuScreen.classList.add("hidden");
+gameScreen.classList.remove("hidden");
+};
+
+botBtn.onclick = () => {
+menuScreen.classList.add("hidden");
+gameScreen.classList.remove("hidden");
+};
+
+onlineBtn.onclick = () => {
+alert("Online mode coming soon");
+};
+
+backBtn.onclick = () => {
+gameScreen.classList.add("hidden");
+menuScreen.classList.remove("hidden");
+};
 
