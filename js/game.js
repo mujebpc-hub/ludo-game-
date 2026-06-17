0b91c366684e77c6e60519ca278131d37ff5f210
mujebpc-hub <mujebpc@gmail.com>
2026-06-17 19:08:46 +0530

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,1 +1,24 @@
+function nextTurn(){
+currentPlayer++;
+
+```
+if(currentPlayer >= players.length){
+    currentPlayer = 0;
+}
+
+console.log("Current Turn:", players[currentPlayer].name);
+```
+
+}
+
+dice.onclick = () => {
+rollDice();
+
+```
+console.log(players[currentPlayer].name + " rolled " + diceValue);
+
+nextTurn();
+```
+
+};
 
