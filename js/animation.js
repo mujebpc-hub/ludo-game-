5b92e0a4be3a068cf5eaa0eeed4a700573dde44d
mujebpc-hub <mujebpc@gmail.com>
2026-06-17 19:20:27 +0530

    Update animation.js

--- js/animation.js
+++ js/animation.js
@@ -1,1 +1,10 @@
+function animateToken(tokenElement, newLeft, newTop){
+tokenElement.style.transition = "all 0.4s ease";
+
+```
+tokenElement.style.left = newLeft + "px";
+tokenElement.style.top = newTop + "px";
+```
+
+}
 
