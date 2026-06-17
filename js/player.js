f1af650276f72329b3ff137d96f50af4b6c6b0ae
mujebpc-hub <mujebpc@gmail.com>
2026-06-17 19:07:44 +0530

    Update player.js

--- js/player.js
+++ js/player.js
@@ -1,1 +1,29 @@
+const players = [
+{
+id: 1,
+name: "Player 1",
+color: "red",
+position: 0,
+tokens: [
+{ id: 1, position: 0 },
+{ id: 2, position: 0 },
+{ id: 3, position: 0 },
+{ id: 4, position: 0 }
+]
+},
+{
+id: 2,
+name: "Player 2",
+color: "blue",
+position: 0,
+tokens: [
+{ id: 1, position: 0 },
+{ id: 2, position: 0 },
+{ id: 3, position: 0 },
+{ id: 4, position: 0 }
+]
+}
+];
+
+let currentPlayer = 0;
 
