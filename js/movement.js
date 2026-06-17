7d3144775369098bd73a742fbdc46a1c363e8cee
mujebpc-hub <mujebpc@gmail.com>
2026-06-17 19:15:46 +0530

    Update movement.js

--- js/movement.js
+++ js/movement.js
@@ -1,1 +1,23 @@
+function moveToken(playerIndex){
+const player = players[playerIndex];
+const token = player.tokens[0]; // first token only for now
+
+```
+token.position += diceValue;
+
+const tokenElement = document.querySelector(
+    `[data-player="${player.id}"][data-token="${token.id}"]`
+);
+
+let currentLeft = parseInt(tokenElement.style.left || 0);
+let currentTop = parseInt(tokenElement.style.top || 0);
+
+// Temporary movement: move right
+currentLeft += diceValue * 30;
+
+tokenElement.style.left = currentLeft + "px";
+tokenElement.style.top = currentTop + "px";
+```
+
+}
 
