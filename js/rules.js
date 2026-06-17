e63bbab02c3af863c0465c3b14e3cc2216f6959f
mujebpc-hub <mujebpc@gmail.com>
2026-06-17 19:18:57 +0530

    Update rules.js

--- js/rules.js
+++ js/rules.js
@@ -1,1 +1,30 @@
+function checkKill(){
+const player1 = players[0].tokens[0];
+const player2 = players[1].tokens[0];
+
+```
+if(player1.position === player2.position && player1.position !== 0){
+    if(currentPlayer === 0){
+        player2.position = 0;
+        console.log("Player 2 token killed!");
+    }else{
+        player1.position = 0;
+        console.log("Player 1 token killed!");
+    }
+}
+```
+
+}
+
+function checkWin(playerIndex){
+const player = players[playerIndex];
+const token = player.tokens[0];
+
+```
+if(token.position >= 50){
+    alert(player.name + " Wins!");
+}
+```
+
+}
 
